12d450e4bdd6e6de0f9e2dc5390f321f290841de
Hector Hansen <116131346+hectorhansen@users.noreply.github.com>
2026-03-17 00:39:42 -0300

    Delete chat directory

--- chat/backend/src/routes/webhook.routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const router = require('express').Router();
-const ctrl = require('../controllers/webhook.controller');
-
-router.get('/', ctrl.verifyWebhook);
-router.post('/', ctrl.receiveWebhook);
-
-module.exports = router;
